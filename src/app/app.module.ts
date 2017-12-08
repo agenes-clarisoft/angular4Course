bd5d915c5c98c877d40d04d625fe0c73c6c0c0f6
agenes-clarisoft <andrei.genes@clarisoft.com>
2017-12-08 15:47:12 +0200

    Http Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './course/course.component';
-import {CoursesService} from "./courses.service";
+import { CoursesService } from "./courses.service";
+import { PostService } from "./services/post.service";
 import {SummaryPipe} from "./summary.pipe";
 import { FavoriteComponent } from './favorite/favorite.component';
 import {TitleCasePipe} from "./title-case.pipe";
@@ -13,6 +15,7 @@ import { PanelComponent } from './panel/panel.component';
 import { LikeComponent } from './like/like.component';
 import { InputFormatDirective } from './input-format.directive';
 import { ZippyComponent } from './zippy/zippy.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
@@ -25,14 +28,17 @@ import { ZippyComponent } from './zippy/zippy.component';
     PanelComponent,
     LikeComponent,
     InputFormatDirective,
-    ZippyComponent
+    ZippyComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
   providers: [
-    CoursesService
+    CoursesService,
+    PostService
   ],
   bootstrap: [AppComponent]
 })
