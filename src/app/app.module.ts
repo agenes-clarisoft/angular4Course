51ee382f083269cd952a92257cee5313291922f0
agenes-clarisoft <andrei.genes@clarisoft.com>
2017-12-08 11:29:48 +0200

    Custom Directive Example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import {TitleCasePipe} from "./title-case.pipe";
 import { PanelComponent } from './panel/panel.component';
 import { LikeComponent } from './like/like.component';
 import { InputFormatDirective } from './input-format.directive';
+import { ZippyComponent } from './zippy/zippy.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { InputFormatDirective } from './input-format.directive';
     FavoriteComponent,
     PanelComponent,
     LikeComponent,
-    InputFormatDirective
+    InputFormatDirective,
+    ZippyComponent
   ],
   imports: [
     BrowserModule,
