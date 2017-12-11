db9170666b8ba8b23d2095e116cfb0b30e1a5719
agenes-clarisoft <andrei.genes@clarisoft.com>
2017-12-11 10:55:38 +0200

    Http Service

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -16,23 +16,23 @@ export class PostsComponent implements OnInit{
   }
 
   ngOnInit() {
-      this.service.getPosts()
-      .subscribe(
-        response => {
-          this.posts = response.json();
-        })
+      this.service.getAll()
+      .subscribe(posts => this.posts = posts);
   }
 
   createPost(input: HTMLInputElement) {
     let post = { title: input.value };
+    this.posts.splice(0, 0, post);
+
     input.value = '';
 
-    this.service.createPost(post)
-      .subscribe(response => {
-        post['id'] = response.json().id;
-        this.posts.splice(0, 0, post);
+    this.service.create(post)
+      .subscribe(newPost => {
+        post['id'] = newPost.id;
       },
         (error: AppError) => {
+          this.posts.splice(0, 1);
+
           if (error instanceof BadInput) {
             //this.form.setErrors(error.originalError);
             alert('An unexpected error occurred.');
@@ -42,20 +42,22 @@ export class PostsComponent implements OnInit{
   }
 
   updatePost(post) {
-    this.service.updatePost(post)
-      .subscribe(response => {
-        console.log(response.json());
+    this.service.update(post)
+      .subscribe(updatedPost => {
+        console.log(updatedPost);
       });
   }
 
   deletePost(post) {
-    this.service.deletePost(345)
+    let index = this.posts.indexOf(post);
+    this.posts.splice(index, 1);
+
+    this.service.delete(345)
       .subscribe(
-        response => {
-          let index = this.posts.indexOf(post);
-          this.posts.splice(index, 1);
-        },
+        null,
         (error: AppError) => {
+          this.posts.splice(index, 0, post);
+
           if (error instanceof NotFoundError)
             alert('this post has already been deleted');
           else throw error;
